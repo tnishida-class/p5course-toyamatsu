ad1c423b73a51dae2f53eec3a8e6dfb39a165ad9
toyamatsu <toya0525@me.com>
2021-10-05 22:42:59 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,12 +5,12 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
-  triangle(12,120,116,120,116,10);
+  fill(178, 10, 162);
+  triangle(12,60,116,120,116,10);
   fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
+  triangle(0,50,106,50,106,0);
   fill(255);
   textSize(32);
   textFont("serif");
-  text("46", 68, 100);
+  text("6", 68, 100);
 }
