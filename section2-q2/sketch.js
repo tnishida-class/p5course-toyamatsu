566c01dbf3639a3067ee66e6797fccad43ce1574
toyamatsu <toya0525@me.com>
2021-10-19 11:52:49 +0900

    update section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,7 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      // 格子を描く
       if(i%2==1 && j%2==1){
         noFill();
         rect(20*i,20*j,20,20);
@@ -18,6 +19,29 @@ function setup() {
         fill(125);
         rect(20*i,20*j,20,20);
       }
+
+      // 円を描く。ゴリ押しで描画してしまった。もっとスマートなやり方があるはず
+      if(i%2==1 && j%2==0 && j<3){
+        fill(255,0,0);
+        ellipse(10+20*i,10+20*j,20);
+      }
+
+      if(i%2==0 && j%2==1 && j<3){
+        fill(255,0,0);
+        ellipse(10+20*i,10+20*j,20);
+      }
+
+      if(i%2==1 && j%2==0 && j>4){
+        fill(0);
+        ellipse(10+20*i,10+20*j,20);
+      }
+
+      if(i%2==0 && j%2==1 && j>4){
+        fill(0);
+        ellipse(10+20*i,10+20*j,20);
+      }
+
     }
   }
+
 }
