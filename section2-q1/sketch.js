07cf20fad15bd93b2c175e694eec370028aa7ed5
toyamatsu <toya0525@me.com>
2021-10-19 12:03:52 +0900

    update section2-q1, 2-q2
    いけてるコードに変更

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -5,12 +5,10 @@ function setup(){
     let r = 10*i+10;
     if(i > 4){
       stroke(255,0,0);
-      ellipse(50,50,r);
     }
     else{
       stroke(0,0,255);
-      ellipse(50,50,r);
     }
-
+    ellipse(50,50,r);
   }
 }
