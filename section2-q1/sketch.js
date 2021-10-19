b55c4cc50f238da2f92e87d083751d6f6b8d8c54
toyamatsu <toya0525@me.com>
2021-10-19 11:19:43 +0900

    update section2-q1 & q2

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    if(i > 5){
+      noFill();
+      stroke(255,0,0);
+      ellipse(50,50,10*i);
+    }
+    else{
+      noFill();
+      stroke(0,0,255);
+      ellipse(50,50,10*i);
+    }
+
   }
 }
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,23 @@
 // チェッカー
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(2000, 2000);
   let size = width / 8;
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if(i%2==1 && j%2==1){
+        noFill();
+        rect(20*i,20*j,20,20);
+      }
+      else if(i%2==1 && j%2==0){
+        fill(125);
+        rect(20*i,20*j,20,20);
+      }
+      else if(i%2==0 && j%2==1){
+        fill(125);
+        rect(20*i,20*j,20,20);
+      }
     }
   }
 }
