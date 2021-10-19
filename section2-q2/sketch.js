0d693eac1b8207b9b3120b5fc830133c0e091558
toyamatsu <toya0525@me.com>
2021-10-19 12:13:47 +0900

    update 2-q2
    20→size

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,45 +1,47 @@
 // チェッカー
 function setup() {
-  createCanvas(2000, 2000);
+  createCanvas(200, 200);
   let size = width / 8;
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
       // 格子を描く
       if(i%2==1 && j%2==1){
         noFill();
-        rect(20*i,20*j,20,20);
+        rect(size*i,size*j,size,size);
       }
       else if(i%2==1 && j%2==0){
         fill(125);
-        rect(20*i,20*j,20,20);
+        rect(size*i,size*j,size,size);
       }
       else if(i%2==0 && j%2==1){
         fill(125);
-        rect(20*i,20*j,20,20);
+        rect(size*i,size*j,size,size);
       }
 
       // 円を描く。ゴリ押しで描画してしまった。もっとスマートなやり方があるはず
+
       if(i%2==1 && j%2==0 && j<3){
         fill(255,0,0);
-        ellipse(10+20*i,10+20*j,18);
+        ellipse(size/2+size*i,size/2+size*j,size);
       }
 
       if(i%2==0 && j%2==1 && j<3){
         fill(255,0,0);
-        ellipse(10+20*i,10+20*j,18);
+        ellipse(size/2+size*i,size/2+size*j,size);
       }
 
       if(i%2==1 && j%2==0 && j>4){
         fill(0);
-        ellipse(10+20*i,10+20*j,18);
+        ellipse(size/2+size*i,size/2+size*j,size);
       }
 
       if(i%2==0 && j%2==1 && j>4){
         fill(0);
-        ellipse(10+20*i,10+20*j,18);
-      }
+        ellipse(size/2+size*i,size/2+size*j,size);
+       }
 
     }
   }
