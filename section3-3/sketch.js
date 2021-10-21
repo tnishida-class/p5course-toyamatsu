3139aed0d09d6cf309a457ba5552f9657ececc9b
toyamatsu <toya0525@me.com>
2021-10-21 19:15:34 +0900

    update section3-q1

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -18,14 +18,14 @@ function draw(){
   if(keyIsDown(" ".charCodeAt(0))){ x-= 10; }
 }
 
-// イベントハンドラを使用するパターン
-// function keyPressed(){
-//   if(keyCode == LEFT_ARROW){ x -= 5; }
-//   else if(keyCode == RIGHT_ARROW){ x+= 5; }
-//   else if(keyCode == DOWN_ARROW){ y += 5; }
-//   else if(keyCode == UP_ARROW){ y -= 5; }
-//   else if(key == "A"){ x += 10; }
-// }
+イベントハンドラを使用するパターン
+function keyPressed(){
+  if(keyCode == LEFT_ARROW){ x -= 5; }
+  else if(keyCode == RIGHT_ARROW){ x+= 5; }
+  else if(keyCode == DOWN_ARROW){ y += 5; }
+  else if(keyCode == UP_ARROW){ y -= 5; }
+  else if(key == "A"){ x += 10; }
+}
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,41 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let x,y,vx,vy;
+const g = 1; // 重力加速度
+const vyMax = 5000;
+
+
 
 function setup(){
-  createCanvas(200, 200);
-  count = 0;
-  cycle = 100;
+  createCanvas(windowWidth, windowHeight);
+  x = width / 2;
+  y = height / 2;
+  vx = 5;
+  vy = 5;
+
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  textSize(50);
+  fill(0);
+  text("WINDOWS",x,y);
+  x += vx
+  y += vy
+
+
+  if(x-10 < 0 || x+250 > width){ vx = -1 * vx; }
+  if(y > height || y-10 < 0){ vy = -1 * vy; }
+  x = constrain(x, 0, width);
+  y = constrain(y, 0, height);
+
+
+
+
+
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
 }
