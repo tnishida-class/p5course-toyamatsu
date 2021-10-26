b9a1921a2955997f5d6554f62d08b38316af170b
toyamatsu <toya0525@me.com>
2021-10-26 11:05:52 +0900

    update section 2-q2
    スマートなコードにした

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -4,46 +4,31 @@ function setup() {
   let size = width / 8;
   noStroke();
   for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
+  for(let j = 0; j < 8; j++){
 
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
       // 格子を描く
-      if(i%2==1 && j%2==1){
+      if((i+j)%2==0){
         noFill();
         rect(size*i,size*j,size,size);
       }
-      else if(i%2==1 && j%2==0){
-        fill(125);
-        rect(size*i,size*j,size,size);
-      }
-      else if(i%2==0 && j%2==1){
+      else{
         fill(125);
         rect(size*i,size*j,size,size);
       }
 
-      // 円を描く。ゴリ押しで描画してしまった。もっとスマートなやり方があるはず
-
-      if(i%2==1 && j%2==0 && j<3){
+      // 円を描く。
+      if((i+j)%2==1 && j<3){
         fill(255,0,0);
         ellipse(size/2+size*i,size/2+size*j,size);
       }
 
-      if(i%2==0 && j%2==1 && j<3){
-        fill(255,0,0);
-        ellipse(size/2+size*i,size/2+size*j,size);
-      }
-
-      if(i%2==1 && j%2==0 && j>4){
+      if((i+j)%2==1 && j>4){
         fill(0);
         ellipse(size/2+size*i,size/2+size*j,size);
       }
 
-      if(i%2==0 && j%2==1 && j>4){
-        fill(0);
-        ellipse(size/2+size*i,size/2+size*j,size);
-       }
-
-    }
+  }
   }
 
 }
