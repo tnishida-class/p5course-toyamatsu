829668993247c3e626d2be12f9e37c0cd399eb2c
toyamatsu <toya0525@me.com>
2021-11-13 08:44:03 +0900

    update section 5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -2,17 +2,19 @@
 // 吹き出しの位置、背景色 etc. を関数 balloon の引数で指定できるようにしてみよう
 // 吹き出しにしっぽを付けてみよう
 function setup(){
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("It is sunny today.");
 }
 
 function balloon(t){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
+  // let p = 2;
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  ellipse(w, 2*h, 2*w, 4*h);
+  // rect(0, 0, w + p * 2, h + p * 2);
+  triangle(5/10*w, 2*h, 7/10*w, 6*h, w, 2*h);
   fill(255);
-  text(t, p, h + p);
+  text(t, w/2, 2*h);
 }
