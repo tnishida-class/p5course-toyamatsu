5171d67fcf09d9af0a8bc35d6e5484b861b150dc
toyamatsu <toya0525@me.com>
2021-11-15 19:53:39 +0900

    update section5-3
    カレンダー表示機能を実装

--- section5-3/sketch.js
+++ section5-3/sketch.js
@@ -1,7 +1,7 @@
 // テキスト「関数を作る(2) 結果を戻す関数」～「総仕上げ：カレンダーを描画しよう」
 function setup(){
-  createCanvas(200, 200);
-  calendar(2019, 10);
+  createCanvas(700, 700);
+  calendar(1900, 11);
 
   // isLeapYear の動作確認のため console に出力しています
   for(let i = 2000; i <= 2100; i++){
@@ -15,9 +15,35 @@ function setup(){
 }
 
 function calendar(y, m){
+  fill(0);
+  noStroke();
+  textSize(32);
+  text(y + "年" + m + "月", 250, 32);
+  fill(255, 0, 0);text("日", 34,70);fill(0);text("月", 134,70);text("火", 234,70);text("水", 334,70);text("木", 434,70);text("金", 534,70);fill(0,0,255);text("土", 634,70);
+    for(i=0; i<7; i++){
+      noFill();
+      stroke(2)
+      rect(i*100, 90, 100, 80);rect(i*100, 170, 100, 80);rect(i*100, 250, 100, 80);rect(i*100, 330, 100, 80);rect(i*100, 410, 100, 80);
+    }
+
   let dow = dayOfWeek(y, m, 1);
+  let x1 = 34 + 100*(dow-1);
+  let y1 = 140;
+
   for(let d = 1; d <= daysInMonth(y, m); d++){
-    // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
+    x1 = x1 + 100;
+    if(x1 > 700){
+      x1 = x1 - 700;
+      y1 = y1 + 80;
+    }
+    if(dayOfWeek(y, m, d) == 0){
+      fill(255,0,0);
+    }else if(dayOfWeek(y, m, d) == 6){
+      fill(0,0,255);
+    }else{
+      fill(0);
+    }
+    text(d, x1, y1);
   }
 }
 
