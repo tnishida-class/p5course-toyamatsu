f921045461ac67b8b5b0c31cb049541e41b2de39
toyamatsu <toya0525@me.com>
2021-11-15 18:16:46 +0900

    update section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,17 +4,18 @@
 function setup(){
   createCanvas(1000, 1000);
   background(255);
-  balloon("It is sunny today.");
+  balloon("It is sunny today.", 200, 33, 45);
 }
 
-function balloon(t){
+function balloon(t, r, g, b){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   // let p = 2;
-  fill(0);
+  noStroke();
+  fill(r,g,b);
+  triangle(5/10*w, 2*h, 7/10*w, 6*h, w, 2*h);
   ellipse(w, 2*h, 2*w, 4*h);
   // rect(0, 0, w + p * 2, h + p * 2);
-  triangle(5/10*w, 2*h, 7/10*w, 6*h, w, 2*h);
   fill(255);
   text(t, w/2, 2*h);
 }
